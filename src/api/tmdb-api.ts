a1f3c9ece99328ad42d331dcd8e3e1542fc3a138
Darina Boinik <darinaboinik@gmail.com>
2025-06-09 19:58:35 +0300

    add all

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -1,21 +1,14 @@
 import axios from 'axios';
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_KEY = import.meta.env.VITE_TMDB_TOKEN; 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-const tmdb = axios.create({
+export const tmdb = axios.create({
   baseURL: BASE_URL,
-headers: {
-Authorization: `Bearer ${API_KEY}`,
-},
+  headers: {
+    Authorization: `Bearer ${API_KEY}`, 
+  },
   params: {
     language: 'en-US',
   },
 });
-
-export const searchMovies = async (query: string) => {
-  const response = await tmdb.get('/search/movie', {
-    params: { query },
-  });
-  return response.data.results;
-};
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,17 +1,11 @@
-import axios from 'axios';
+import { tmdb } from '../api/tmdb-api';
 import type { MoviesResponse } from '../types/movie';
 
-const API_URL = 'https://api.themoviedb.org/3/search/movie';
-const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
-
 export const fetchMovies = async (query: string, page: number): Promise<MoviesResponse> => {
-  const response = await axios.get<MoviesResponse>(API_URL, {
+  const response = await tmdb.get<MoviesResponse>('/search/movie', {
     params: {
       query,
       page,
-    },
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
     },
   });
 
